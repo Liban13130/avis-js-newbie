affc969792fd1f1a45af2978ac2b7e5c71c60a17
Nabil Hellal <nabil-13130@hotmail.fr>
2024-03-09 00:26:33 +0100

    Finito

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 let submit = document.querySelector(".submit");
+let buttons = document.querySelectorAll(".button")
+let thankYouCard = document.querySelector('.container-2')
+let ratingCard = document.querySelector('.container')
+let choice = document.getElementById("choice")
+
+let rating;
+
+buttons.forEach(button => {
+    button.addEventListener("click", () =>{
+        rating = button.textContent
+    })
+})
 
 submit.addEventListener("click", () =>{
-    console.log("Je me clique");
+    if(rating >= 0){
+        thankYouCard.style.display = "flex"
+        ratingCard.style.display = "none"
+        choice.textContent = `You selected ${rating} out of 5`
+    }else{
+        return alert("Please make a choice.")
+    }
+    
 })
